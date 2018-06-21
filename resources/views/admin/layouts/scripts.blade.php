f2f3019dd887728fb215c8cb33ed53785dc6645f
Pavel Basenko <p.basenko@simcord.com>
2018-06-21 10:07:35 +0300

    Начал прикручивать ElaAdmin admin template на сайт. Готова форма логина.

--- resources/views/admin/layouts/scripts.blade.php
+++ resources/views/admin/layouts/scripts.blade.php
@@ -1,1 +1,15 @@
-<script src="{{ asset('admin/js/script.js') }}"></script>
+<!--<script src="{{ asset('admin/js/script.js') }}"></script>-->
+
+<!-- All Jquery -->
+    <script src="js/lib/jquery/jquery.min.js"></script>
+    <!-- Bootstrap tether Core JavaScript -->
+    <script src="js/lib/bootstrap/js/popper.min.js"></script>
+    <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
+    <!-- slimscrollbar scrollbar JavaScript -->
+    <script src="js/jquery.slimscroll.js"></script>
+    <!--Menu sidebar -->
+    <script src="js/sidebarmenu.js"></script>
+    <!--stickey kit -->
+    <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+    <!--Custom JavaScript -->
+    <script src="js/scripts.js"></script>
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,56 +1,57 @@
 <!DOCTYPE html>
-<html>
-    @include('admin.layouts.htmlhead')
-
-    <body>
-        <div class="login-wrap customscroll d-flex align-items-center flex-wrap justify-content-center pd-20">
-            <div class="login-box bg-white box-shadow pd-30 border-radius-5">
-                <img src="{{ asset('admin/images/login-img.png') }}" alt="login" class="login-img">
-                <h2 class="text-center mb-30">Login</h2>
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
-                    
-                    <div class="input-group custom input-group-lg">
-                        <input type="text" id="login" class="form-control{{ $errors->has('login') ? ' is-invalid' : '' }}" name="login" value="{{ old('login') }}" placeholder="Login" required autofocus>
-                        @if ($errors->has('login'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('login') }}</strong>
-                            </span>
-                        @endif
-                        
-                        <div class="input-group-append custom">
-                            <span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
-                        </div>
-                    </div>
-                    <div class="input-group custom input-group-lg">
-                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password" name="password" required>
-                        @if ($errors->has('password'))
-                            <span class="invalid-feedback">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                        
-                        <div class="input-group-append custom">
-                            <span class="input-group-text"><i class="fa fa-lock" aria-hidden="true"></i></span>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="custom-control custom-checkbox mb-5">
-				<input class="custom-control-input" id="remember" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}>
-				<label class="custom-control-label" for="remember">{{ __('Remember Me') }}</label>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="input-group">
-                                <input class="btn btn-outline-primary btn-lg btn-block" type="submit" value="{{ __('Login') }}">
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
+<html lang="ru">
+    @include('admin.layouts.htmlhead')    
+    <body class="fix-header fix-sidebar">
+        <div class="preloader">
+            <svg class="circular" viewBox="25 25 50 50">
+            <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
         </div>
+        <div id="main-wrapper">
 
+            <div class="unix-login">
+                <div class="container-fluid">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-4">
+                            <div class="login-content card">
+                                <div class="login-form">
+                                    <h4>Login</h4>
+                                    <form method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        
+                                        <div class="form-group">
+                                            <label>Логин</label>
+                                            <input type="text" id="login" class="form-control{{ $errors->has('login') ? ' is-invalid' : '' }}" name="login" value="{{ old('login') }}" placeholder="Логин" required autofocus>
+                                            @if ($errors->has('login'))
+                                                <span class="invalid-feedback">
+                                                    <strong>{{ $errors->first('login') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Password</label>
+                                            <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password" name="password" required>
+                                            @if ($errors->has('password'))
+                                                <span class="invalid-feedback">
+                                                    <strong>{{ $errors->first('password') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="checkbox">
+                                            <label>
+                                                <input class="custom-control-input" id="remember" type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Запомнить
+                                            </label>
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-flat m-b-30 m-t-30">Войти</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        
         @include('admin.layouts.scripts')
     </body>
 </html>
