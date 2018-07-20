d057c15f397ac6e6f75b0eb3b285cf6cd04c2682
Pashaster12 <pashaster1207@gmail.com>
2018-07-20 14:30:49 +0300

    Подключение CSS-скриптов через Laravel хэлперы

--- resources/views/admin/layouts/styles.blade.php
+++ resources/views/admin/layouts/styles.blade.php
@@ -1,5 +1,5 @@
-<link href="/admin/css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
-<link href="/admin/css/helper.css" rel="stylesheet">
-<link href="/admin/css/style.css" rel="stylesheet">
+<link href="{{ asset('/admin/css/lib/bootstrap/bootstrap.min.css') }}" rel="stylesheet">
+<link href="{{ asset('/admin/css/helper.css') }}" rel="stylesheet">
+<link href="{{ asset('/admin/css/style.css') }}" rel="stylesheet">
 
 @stack('custom_styles')
