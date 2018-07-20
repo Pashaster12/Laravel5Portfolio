9059485625abc3ca8c6961ae536a690cd36f60a2
Pashaster12 <pashaster1207@gmail.com>
2018-07-20 14:32:26 +0300

    Подключение JS-скриптов через Laravel хэлперы

--- resources/views/admin/layouts/scripts.blade.php
+++ resources/views/admin/layouts/scripts.blade.php
@@ -1,9 +1,9 @@
-<script src="/admin/js/lib/jquery/jquery.min.js"></script>
-<script src="/admin/js/lib/bootstrap/js/popper.min.js"></script>
-<script src="/admin/js/lib/bootstrap/js/bootstrap.min.js"></script>
-<script src="/admin/js/jquery.slimscroll.js"></script>
-<script src="/admin/js/sidebarmenu.js"></script>
-<script src="/admin/js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
-<script src="/admin/js/scripts.js"></script>
+<script src="{{ asset('/admin/js/lib/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('/admin/js/lib/bootstrap/js/popper.min.js') }}"></script>
+<script src="{{ asset('/admin/js/lib/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('/admin/js/jquery.slimscroll.js') }}"></script>
+<script src="{{ asset('/admin/js/sidebarmenu.js') }}"></script>
+<script src="{{ asset('/admin/js/lib/sticky-kit-master/dist/sticky-kit.min.js') }}"></script>
+<script src="{{ asset('/admin/js/scripts.js') }}"></script>
 
 @stack('custom_scripts')
